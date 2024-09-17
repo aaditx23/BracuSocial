7ff8b94989bc2c92387b33b2ef6a687391cef32e
aaditx23 <adit.pranto@gmail.com>
2024-09-17 13:55:25 +0600

    Fix: Time slot variables minor fix

--- app/src/main/java/com/aaditx23/bracusocial/Global.kt
+++ app/src/main/java/com/aaditx23/bracusocial/Global.kt
@@ -11,19 +11,19 @@ operator fun <T> List<T>.component6(): T = get(5)
 operator fun <T> List<T>.component7(): T = get(6)
 operator fun <T> List<T>.component8(): T = get(7)
 val timeSlots = listOf(
-    "08:00 AM - 09:20 AM",
-    "08:00 AM - 10:50 AM", //lab
-    "09:30 AM - 10:50 AM",
-    "11:00 AM - 12:20 PM",
-    "11:00 AM - 01:50 PM", //lab
+    "08:00 AM-09:20 AM",
+    "08:00 AM-10:50 AM", //lab
+    "09:30 AM-10:50 AM",
+    "11:00 AM-12:20 PM",
+    "11:00 AM-01:50 PM", //lab
 
-    "12:30 PM - 01:50 PM",
-    "02:00 PM - 03:20 PM",
-    "02:00 PM - 04:50 PM", //lab
-    "03:30 PM - 04:50 PM",
-    "05:00 PM - 06:20 PM",
-    "05:00 PM - 08:00 PM", //lab
-    "06:30 PM - 08:00 PM",
+    "12:30 PM-01:50 PM",
+    "02:00 PM-03:20 PM",
+    "02:00 PM-04:50 PM", //lab
+    "03:30 PM-04:50 PM",
+    "05:00 PM-06:20 PM",
+    "05:00 PM-08:00 PM", //lab
+    "06:30 PM-08:00 PM",
     "Closed"
 )
 val days = listOf(
@@ -53,21 +53,21 @@ fun listfiltersAddAll(list: List<String>, item: String): List<String>{
 
 
 val classTimeList = listOf(
-    "08:00 AM - 09:20 AM",
-    "09:30 AM - 10:50 AM",
-    "11:00 AM - 12:20 PM",
-    "12:30 PM - 01:50 PM",
-    "02:00 PM - 03:20 PM",
-    "03:30 PM - 04:50 PM",
-    "05:00 PM - 06:20 PM",
-    "06:30 PM - 08:00 PM",
+    "08:00 AM-09:20 AM",
+    "09:30 AM-10:50 AM",
+    "11:00 AM-12:20 PM",
+    "12:30 PM-01:50 PM",
+    "02:00 PM-03:20 PM",
+    "03:30 PM-04:50 PM",
+    "05:00 PM-06:20 PM",
+    "06:30 PM-08:00 PM",
     "Closed"
 )
 val labTimeList = listOf(
-    "08:00 AM - 10:50 AM",
-    "11:00 AM - 01:50 PM",
-    "02:00 PM - 04:50 PM",
-    "05:00 PM - 08:00 PM",
+    "08:00 AM-10:50 AM",
+    "11:00 AM-01:50 PM",
+    "02:00 PM-04:50 PM",
+    "05:00 PM-08:00 PM",
     "Closed"
 )
 
