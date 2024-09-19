98c7706dd9398e2622941224f0eabd8c527270cf
aaditx23 <adit.pranto@gmail.com>
2024-09-19 21:25:09 +0600

    Chore: Started alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "alpha_01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
