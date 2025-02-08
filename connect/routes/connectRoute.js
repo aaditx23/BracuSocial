0a3938fd2a1b1f3e329c9c37ceb1ed13dc83a36f
aaditx23 <adit.pranto@gmail.com>
2025-02-08 22:44:55 +0600

    Chore: Test api

--- connect/routes/connectRoute.js
+++ connect/routes/connectRoute.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const connectC = require('../controller/connectController');
 
+
 router.post('/connect/login', connectC.login);
+router.get('/connect/get', (req, res)=> {
+    res.json({ message: 'Data retrieved successfully' });
+})
 
 
 module.exports = router;
